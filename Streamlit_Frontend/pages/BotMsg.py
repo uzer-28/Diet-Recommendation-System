a3f1e3a6ad97734bf52ffda42272048e186ea9a7
Mohammed Uzer Qazi <108578286+uzer-28@users.noreply.github.com>
2026-01-12 17:00:20 +0530

    Update BotMsg.py

--- Streamlit_Frontend/pages/BotMsg.py
+++ Streamlit_Frontend/pages/BotMsg.py
@@ -4,14 +4,13 @@ from telegram import Bot
 # Replace with your bot token and chat ID
 
 class TelegramBot:
-    # '7583254091:AAE4rkEJAPZEquWvjlEtEfA4cgQzlMJQVak'
-    # '1936587709'
     def __init__(self):
-        self.bot = Bot(token='7583254091:AAE4rkEJAPZEquWvjlEtEfA4cgQzlMJQVak')
-        self.chat_id = 1936587709
+        self.bot = Bot(token='')
+        self.chat_id = 
 
     async def send_message(self, text):
         await self.bot.send_message(chat_id=self.chat_id, text=text)
 
     def run(self, text):
+
         asyncio.run(self.send_message(text))
